dffb71ce36778f12249cee9a44c275da1f35b45e
KonstantinVolodko <faker2000311@gmail.com>
2023-11-08 16:15:35 +0300

    fix content

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -19,13 +19,11 @@ document.addEventListener("DOMContentLoaded", () => {
       acc[i].addEventListener("click", function () {
         let currentlyActiveAccordion = document.querySelector('.accordion.active');
         let panel = this.nextElementSibling;
-        let allPanels = document.querySelectorAll('.panel');
-        let allPanelTexts = document.querySelectorAll('.panel p');
 
         if (window.matchMedia("(min-width: 500px)").matches && currentlyActiveAccordion && currentlyActiveAccordion !== this) {
           let activePanel = currentlyActiveAccordion.nextElementSibling;
-          let activeTexts = activePanel.querySelectorAll('p');
-          activeTexts.forEach(text => text.classList.remove('active-text'));
+          let activeElements = activePanel.querySelectorAll('*');
+          activeElements.forEach(el => el.classList.remove('active-text'));
           currentlyActiveAccordion.classList.remove("active");
           activePanel.style.maxHeight = null;
           accordeonContainer.style.height = initialHeight + "px";
@@ -37,10 +35,10 @@ document.addEventListener("DOMContentLoaded", () => {
           panel.style.maxHeight = null;
           panel.style.marginTop = "0px"
           if (window.matchMedia("(min-width: 500px)").matches) {
-            allPanelTexts.forEach(text => text.classList.remove('active-text'));
+            let allElements = panel.querySelectorAll('*');
+            allElements.forEach(el => el.classList.remove('active-text'));
             accordeonContainer.style.height = initialHeight + "px";
           }
-
         } else {
           panel.style.maxHeight = panel.scrollHeight + "px";
           panel.style.marginTop = "25px"
@@ -48,24 +46,22 @@ document.addEventListener("DOMContentLoaded", () => {
           if (window.matchMedia("(min-width: 500px)").matches) {
             panel.style.marginTop = "0px"
             setTimeout(() => {
-              let texts = panel.querySelectorAll('p');
-              texts.forEach(text => text.classList.add('active-text'));
+              let elements = panel.querySelectorAll('*');
+              elements.forEach(el => el.classList.add('active-text'));
             }, 200);
             accordeonContainer.style.height = panel.scrollHeight + "px";
-            allPanels.forEach(p => {
+            document.querySelectorAll('.panel').forEach(p => {
               if (p !== panel) {
                 p.style.maxHeight = null;
-                p.querySelectorAll('p').forEach(text => text.classList.remove('active-text'));
+                p.querySelectorAll('*').forEach(el => el.classList.remove('active-text'));
               }
             });
           }
         }
-
-        
       });
     }
 
-    acc[0].click()
+    acc[0].click();
   }
 
 })
